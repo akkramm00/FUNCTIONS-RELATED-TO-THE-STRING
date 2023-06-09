5c563233dcb541219746b85069a83902cf56b568
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-09 11:55:52 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+// String length => strlen() :
+
+
+
+
+
+
+
+  ?> 
+
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
